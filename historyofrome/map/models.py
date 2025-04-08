39e2c10d0af97d9d965a551f048b712a893222d1
Spencer <wutang4children@gmail.com>
2025-04-07 21:20:39 -0700

    stub out event models

--- historyofrome/map/models.py
+++ historyofrome/map/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class PointEvent(models.Model):
+    """used for tracking a single discrete event"""
+    summary = models.CharField()
+    details = models.TextField()
+    location = models.PointField()
+    date = models.DateField()
+
+class RangeEvent(models.Model):
+    """used for tracking extented events"""
+    summary = models.CharField()
+    details = models.TextField()
+    location = models.PolygonField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+# class BaseMap(models.Model)
+# class Boundary(models.Model)
