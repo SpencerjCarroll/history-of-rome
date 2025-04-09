db8a68f468658876b3556622d1437bfe4d483992
Spencer <wutang4children@gmail.com>
2025-04-08 21:17:47 -0700

    more model work

--- historyofrome/map/models.py
+++ historyofrome/map/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-class PointEvent(models.Model):
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'  # Use email as the unique identifier
+    REQUIRED_FIELDS = []  # Remove username from required fields
+    
+class Event(models.Model):
     """used for tracking a single discrete event"""
-    summary = models.CharField()
-    details = models.TextField()
-    location = models.PointField()
-    date = models.DateField()
+    summary = models.CharField(blank=True, null=True, max_length=50)
+    details = models.TextField(null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    type = models.CharField(blank=True, null=True, max_length=50)
 
-class RangeEvent(models.Model):
-    """used for tracking extented events"""
-    summary = models.CharField()
-    details = models.TextField()
-    location = models.PolygonField()
-    start_date = models.DateField()
-    end_date = models.DateField()
+class PointEvent(Event):
+    location = models.PointField()
+
+class PolyEvent(Event):
+    bbox = models.PolygonField()
 
 # class BaseMap(models.Model)
 # class Boundary(models.Model)
